187235209f06755004b1a2507c5ee6acf305c96d
Nisal <it21259302@my.sliit.lk>
2023-12-14 22:15:10 +0530

    header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,24 +1,36 @@
 import React from "react";
+import { NavLinks } from "../helpers/Header";
 
 const Header = () => {
+  const navigationLinks = [
+    {
+      name: "SERVICES",
+      url: "#",
+    },
+    {
+      name: "ABOUT US",
+      url: "#",
+    },
+    {
+      name: "CONTACT US",
+      url: "#",
+    },
+    {
+      name: "CAREERS",
+      url: "#",
+    },
+  ];
   return (
-    <header className='bg-primary-color p-4'>
+    <header className='bg-primary-color'>
       <div className='container mx-auto flex items-center justify-between'>
-        <div className='text-white text-2xl font-bold'>Your Logo</div>
-        <nav className='space-x-4'>
-          <a href='#' className='text-white hover:underline'>
-            Home
-          </a>
-          <a href='#' className='text-white hover:underline'>
-            About
-          </a>
-          <a href='#' className='text-white hover:underline'>
-            Services
-          </a>
-          <a href='#' className='text-white hover:underline'>
-            Contact
-          </a>
-        </nav>
+        <div className='m-4'>
+          <img
+            className='w-logo h-logo'
+            src='images/header/White-Logo.png'
+            alt='logo'
+          />
+        </div>
+        <NavLinks links={navigationLinks} />
       </div>
     </header>
   );
