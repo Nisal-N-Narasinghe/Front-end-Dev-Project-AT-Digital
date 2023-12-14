8d5b76148e0a51e0fe412d96815b7713b3762870
Nisal <it21259302@my.sliit.lk>
2023-12-14 19:49:37 +0530

    header

---
+++ front-end-dev-project/src/components/Header.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className='bg-blue-500 p-4'>
+      <div className='container mx-auto flex items-center justify-between'>
+        <div className='text-white text-2xl font-bold'>Your Logo</div>
+        <nav className='space-x-4'>
+          <a href='#' className='text-white hover:underline'>
+            Home
+          </a>
+          <a href='#' className='text-white hover:underline'>
+            About
+          </a>
+          <a href='#' className='text-white hover:underline'>
+            Services
+          </a>
+          <a href='#' className='text-white hover:underline'>
+            Contact
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
